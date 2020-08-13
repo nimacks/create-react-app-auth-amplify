78844924b32c95ebc0dd0e7b3d32dce02d475d31
hspain <hspain@gmail.com>
2020-08-12 18:45:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          V46 dev
+          V47 dev
           </p>
           <a
             className="App-link"
